2c7eb07697cb91c5354a4ddc60f053183a6fbc49
LF3551 <adk3551@gmail.com>
2022-05-11 09:17:01 +0300

    test

--- ex4.go
+++ ex4.go
@@ -5,5 +5,6 @@ import "fmt"
 func main() {
     slice1 := []int{1,2,3}
     slice2 := append(slice1, 4, 5)
-    fmt.Println(slice1, slice2)
+	slice3 := append(slice2, 8,9)
+    fmt.Println(slice1, slice2,slice3)
 }
